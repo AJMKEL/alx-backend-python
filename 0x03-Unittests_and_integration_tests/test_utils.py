a4edf213be401c9795f325ee79e73f52c0822c1b
Johnson Aboagye <johnsonaboagyeaj@gmail.com>
2025-11-30 02:27:14 +0000

    Update test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -6,7 +6,7 @@ from unittest.mock import patch, Mock
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """Test access_nested_map"""
+    """Test access_nested_map function"""
 
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
@@ -23,25 +23,28 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map_exception(self, nested_map, path):
         with self.assertRaises(KeyError) as e:
             access_nested_map(nested_map, path)
+
+        # KeyError message should match missing key, not tuple
         self.assertEqual(str(e.exception), repr(path[-1]))
 
 
 class TestGetJson(unittest.TestCase):
-    """Test get_json"""
+    """Test get_json function"""
 
     @parameterized.expand([
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False}),
     ])
     @patch("utils.requests.get")
-    def test_get_json(self, url, payload, mock_get):
+    def test_get_json(self, test_url, test_payload, mock_get):
         mock_resp = Mock()
-        mock_resp.json.return_value = payload
+        mock_resp.json.return_value = test_payload
         mock_get.return_value = mock_resp
 
-        result = get_json(url)
-        self.assertEqual(result, payload)
-        mock_get.assert_called_once_with(url)
+        result = get_json(test_url)
+        self.assertEqual(result, test_payload)
+
+        mock_get.assert_called_once_with(test_url)
 
 
 class TestMemoize(unittest.TestCase):
@@ -61,4 +64,5 @@ class TestMemoize(unittest.TestCase):
         with patch.object(obj, "a_method", return_value=42) as mock_method:
             self.assertEqual(obj.a_property, 42)
             self.assertEqual(obj.a_property, 42)
+
             mock_method.assert_called_once()
